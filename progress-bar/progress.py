0e6039a9260b405baefa54f664e1aa77eb54085e
“Pratik <patel.parth200@gmail.com>
2020-05-19 08:27:31 -0500

    progress bar sample

--- progress-bar/progress.py
+++ progress-bar/progress.py
@@ -1,25 +1,14 @@
 # This file contains different progress bar tests.
-# option 1 : https://github.com/tqdm/tqdm
-#option 2 : https://pypi.org/project/progress/ --> lot more options.
-  #to use this, pip install progress
+# option 1 : https://github.com/tqdm/tqdm, https://pypi.org/project/tqdm/
+# to use this, pip install progress
 from tqdm import tqdm
-from progress.bar import Bar
 import time
 
 
 def progress_check_tqdm(custom_range):
-  for i in tqdm(range(custom_range)):
-    time.sleep(1)
+    for i in tqdm(range(custom_range)):
+        time.sleep(1)
 
-#from: https://pypi.org/project/progress/
-def progress_check_pypiprogress(custom_range):
-  bar = Bar('Processing', max=custom_range)
-  for i in range(custom_range):
-    # Do somework
-    time.sleep(1)
-    bar.next()
-  bar.finish()
 
-#progress_check_tqdm(25)
-progress_check_pypiprogress(25)
+progress_check_tqdm(25)
 
